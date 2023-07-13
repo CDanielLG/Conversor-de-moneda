ea9dc18414e76dc7adae0b40c5349247b16f923c
CDanielLG <hikivel139@gmail.com>
2023-07-12 19:09:57 -0600

    API aplicada correctamente a moneda

--- src/conversor/Moneda.java
+++ src/conversor/Moneda.java
@@ -17,6 +17,7 @@ import javax.swing.JButton;
 
 //Importar clases para la APi conversion
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -38,70 +39,29 @@ public class Moneda extends JPanel {
             "GBP - Libra Esterlina británica"
         };
   
-    String apiKey = "a87b9818de26537a0cb94e0c63c71a9d";
-    String apiUrl = "https://api.exchangeratesapi.io/v1/latest?access_key=" + apiKey;
+    String apiKey = "1080300593cf9a67920fcddc";
+    String apiUrl = "https://v6.exchangerate-api.com/v6/";
 
 
 
     public Moneda() {
    
+    	try {
+    	    URL url = new URL(apiUrl + apiKey + "latest/USD");
+    	    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    	    connection.setRequestMethod("GET");
 
-        // Definir los factores de conversión entre cada par de monedas
-        
-//        tiposDeCambio = new HashMap<>();
-//        Map<String, Double> conversionesUSD = new HashMap<>();
-//        conversionesUSD.put("MXN - Pesos Mexicanos", 16.87);
-//        conversionesUSD.put("USD - Dólares Estadounidenses", 1.0);
-//        conversionesUSD.put("EUR - Euro", 0.89);
-//        conversionesUSD.put("JPY - Yen Japonés", 138.205);
-//        conversionesUSD.put("KRW - Won Surcoreano", 1275.63);
-//        conversionesUSD.put("GBP - Libra Esterlina britanica", 0.77);
-//        tiposDeCambio.put("USD - Dólares Estadounidenses", conversionesUSD);
-//
-//        Map<String, Double> conversionesEUR = new HashMap<>();
-//        conversionesEUR.put("MXN - Pesos Mexicanos", 18.79);
-//        conversionesEUR.put("USD - Dólares Estadounidenses", 1.11);
-//        conversionesEUR.put("EUR - Euro", 1.0);
-//        conversionesEUR.put("JPY - Yen Japonés", 154.03);
-//        conversionesEUR.put("KRW - Won Surcoreano", 1419.6);
-//        conversionesUSD.put("GBP - Libra Esterlina britanica", 0.85);
-//        tiposDeCambio.put("EUR - Euro", conversionesEUR);
-//        
-//        Map<String, Double> conversionesMXN = new HashMap<>();
-//        conversionesMXN.put("MXN - Pesos Mexicanos", 1.00);
-//        conversionesMXN.put("USD - Dólares Estadounidenses", 0.05858);
-//        conversionesMXN.put("EUR - Euro", 0.05);
-//        conversionesMXN.put("JPY - Yen Japonés", 8.1);
-//        conversionesMXN.put("KRW - Won Surcoreano", 75.5);
-//        conversionesMXN.put("GBP - Libra Esterlina britanica", 0.04);
-//        tiposDeCambio.put("MXN - Pesos Mexicanos", conversionesMXN);
-//        
-//        Map<String, Double> conversionesKRW = new HashMap<>();
-//        conversionesKRW.put("MXN - Pesos Mexicanos",  0.01);
-//        conversionesKRW.put("USD - Dólares Estadounidenses", 0.0007);
-//        conversionesKRW.put("EUR - Euro", 0.0007);
-//        conversionesKRW.put("JPY - Yen Japonés", 0.10);
-//        conversionesKRW.put("KRW - Won Surcoreano", 1.0);
-//        conversionesKRW.put("GBP - Libra Esterlina britanica", 0.0006);
-//        tiposDeCambio.put("KRW - Won Surcoreano", conversionesKRW);
-//        
-//        Map<String, Double> conversionesJPY = new HashMap<>();
-//        conversionesJPY.put("MXN - Pesos Mexicanos", 0.12);
-//        conversionesJPY.put("USD - Dólares Estadounidenses", 1.18);
-//        conversionesJPY.put("EUR - Euro", 1.0);
-//        conversionesJPY.put("JPY - Yen Japonés", 1.00);
-//        conversionesJPY.put("KRW - Won Surcoreano", 9.21);
-//        conversionesJPY.put("GBP - Libra Esterlina britanica", 0.006);
-//        tiposDeCambio.put("JPY - Yen Japonés", conversionesJPY);
-//        
-//        Map<String, Double> conversionesGBP = new HashMap<>();
-//        conversionesGBP.put("MXN - Pesos Mexicanos", 21.92);
-//        conversionesGBP.put("USD - Dólares Estadounidenses", 1.29);
-//        conversionesGBP.put("EUR - Euro", 1.16);
-//        conversionesGBP.put("JPY - Yen Japonés", 179.72);
-//        conversionesGBP.put("KRW - Won Surcoreano", 1656.33);
-//        conversionesGBP.put("GBP - Libra Esterlina britanica", 1.00);
-//        tiposDeCambio.put("GBP - Libra Esterlina britanica", conversionesGBP);
+    	    int responseCode = connection.getResponseCode();
+    	    if (responseCode == HttpURLConnection.HTTP_OK) {
+    	        System.out.println("Conexión exitosa a la API");
+    	        // Puedes realizar otras operaciones con la respuesta de la API aquí
+    	    } else {
+    	        System.out.println("No se pudo conectar a la API. Código de respuesta: " + responseCode);
+    	    }
+    	} catch (IOException e) {
+    	    System.out.println("Error al conectar a la API: " + e.getMessage());
+    	}
+
 
         setLayout(null);
 
@@ -169,9 +129,9 @@ public class Moneda extends JPanel {
         }
         String monedaDestino = (String) opcionesConversion.getSelectedItem();
         
-        double factorConversion = obtenerFactorConversion(monedaOrigen, monedaDestino);
+        double factorConversion = obtenerFactorConversion(cantidad,monedaOrigen, monedaDestino);
         if (factorConversion == -1.0) {
-            resultadoLbl.setText("Resultado: No se encontró el tipo de cambio para convertir de " + monedaOrigen + " a " + monedaDestino);
+            resultadoLbl.setText("Resultado: No se encontró el tipo de cambio para convertir de "+ monedaOrigen + " a " + monedaDestino);
             return;
         }
 
@@ -181,39 +141,13 @@ public class Moneda extends JPanel {
         String resultadoTexto = "Resultado: " + cantidad + " " + monedaOrigen + " = " + resultadoFormateado + " " + monedaDestino;
         resultadoLbl.setText(resultadoTexto);
 
-      //  Map<String, Double> conversionesOrigen = tiposDeCambio.get(monedaOrigen);
-//        if (conversionesOrigen == null) {
-//            resultadoLbl.setText("Resultado: No se encontró el tipo de cambio para la moneda de origen");
-//            return;
-//        }
 
-//        Double factorConversion = conversionesOrigen.get(monedaDestino);
-//        if (factorConversion == null) {
-//            resultadoLbl.setText("Resultado: No se encontró el tipo de cambio para convertir de " + monedaOrigen + " a " + monedaDestino);
-//            return;
-//        }
-
-//        double resultado = cantidad * factorConversion;
-//        String resultadoFormateado = String.format("%.2f", resultado);
-//
-//        String resultadoTexto = "Resultado: " + cantidad + " " + monedaOrigen + " = " + resultadoFormateado + " " + monedaDestino;
-//        resultadoLbl.setText(resultadoTexto);
     }
 
-//    public double convertir(double cantidad, String monedaOrigen, String monedaDestino) {
-//    	   if (monedaOrigen.equals(monedaDestino)) {
-//               return cantidad; // No se requiere conversión si las monedas son iguales
-//           }
-//
-//           double factorConversion = obtenerFactorConversion(monedaOrigen, monedaDestino);
-//           if (factorConversion == -1.0) {
-//               throw new IllegalArgumentException("No se encontró el tipo de cambio para convertir de " + monedaOrigen + " a " + monedaDestino);
-//           }
-//
-//        return cantidad * factorConversion;
-//    }
-    private double obtenerFactorConversion(String monedaOrigen, String monedaDestino) {
-        String url = "https://api.exchangeratesapi.io/v1/latest/" + monedaOrigen.substring(0, 3);
+
+    private double obtenerFactorConversion(double cantidad, String monedaOrigen, String monedaDestino) {
+        String apiKey = "1080300593cf9a67920fcddc"; // Reemplaza "TU_CLAVE_DE_API" por tu clave de API válida
+        String url = "https://v6.exchangerate-api.com/v6/" + apiKey + "/pair/" + monedaOrigen.substring(0, 3) + "/" + monedaDestino.substring(0, 3);
 
         try {
             URL apiUrl = new URL(url);
@@ -232,10 +166,12 @@ public class Moneda extends JPanel {
 
                 // Parsear el JSON de respuesta para obtener el factor de conversión
                 JSONObject jsonResponse = new JSONObject(response.toString());
-                JSONObject rates = jsonResponse.getJSONObject("rates");
-                if (rates.has(monedaDestino)) {
-                    return rates.getDouble(monedaDestino.substring(0, 3));
-                }
+                System.out.println("Respuesta JSON: " + response.toString());
+                double resultado = jsonResponse.getDouble("conversion_rate");
+                return resultado;
+            } else {
+                // Error en la respuesta de la API
+                System.out.println("Error en la respuesta de la API: " + responseCode);
             }
         } catch (Exception e) {
             e.printStackTrace();
